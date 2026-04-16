2e50f4bcc03124ec37f2a8c74d0b85013aba9bc0
Chandra Shekhar <imchandra8888@gmail.com>
2026-04-16 17:52:41 +0530

    changes 3

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 export default {
+  // Make sure to add your 'content' paths here if you haven't already!
   theme: {
     extend: {
       fontFamily: {
         aileron: ['Aileron', 'sans-serif'],
+        work: ['"Work Sans"', 'sans-serif'],
       },
     },
   },
+  plugins: [],
 };
